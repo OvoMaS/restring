94b04ae0d46a21307a5812cfaad9ba6e442d3855
Marco Scavo <marco.scavo@ovolab.com>
2023-07-25 15:01:03 +0200

    Reproduce webview crash

--- example/src/main/java/dev/b3nedikt/restring/example/MainFragment.kt
+++ example/src/main/java/dev/b3nedikt/restring/example/MainFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
@@ -26,24 +27,7 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    private lateinit var spinner: Spinner
-
-    private lateinit var stringArrayTextView: TextView
-    private lateinit var quantityStringTextView: TextView
-
-    private lateinit var stringNotInStringsXmlTextView: TextView
-    private lateinit var stringFromApplicationContextTextView: TextView
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        spinner = view.findViewById(R.id.spinner)
-
-        stringArrayTextView = view.findViewById(R.id.stringArrayTextView)
-        quantityStringTextView = view.findViewById(R.id.quantityStringTextView)
-
-        stringNotInStringsXmlTextView = view.findViewById(R.id.stringNotInStringsXmlTextView)
-        stringFromApplicationContextTextView =
-            view.findViewById(R.id.stringFromApplicationContextTextView)
 
         AppLocale.supportedLocales.forEach { locale ->
             Restring.putStrings(locale, ExampleStringsGenerator.getStrings(locale))
@@ -51,60 +35,9 @@ class MainFragment : Fragment() {
             Restring.putStringArrays(locale, ExampleStringsGenerator.getStringArrays(locale))
         }
 
-        val localeStrings = AppLocale.supportedLocales.map { it.language + " " + it.country }
-        val adapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_dropdown_item_1line, localeStrings
-        )
+        val webView = view.findViewById<WebView>(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select")
 
-        spinner.adapter = adapter
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                AppLocale.desiredLocale = AppLocale.supportedLocales[position]
-
-                val rootView =
-                    requireActivity()
-                        .window
-                        .decorView
-                        .findViewById<ContentFrameLayout>(android.R.id.content)
-
-                Reword.reword(rootView)
-
-                stringArrayTextView.text = resources.getStringArray(R.array.string_array)
-                        .joinToString("\n")
-
-                quantityStringTextView.text = (0 until 3)
-                    .joinToString("\n")
-                    { resources.getQuantityString(R.plurals.quantity_string, it, it) }
-
-                stringNotInStringsXmlTextView.text = requireContext()
-                    .getStringResourceByName("a_string_not_in_strings_xml")
-
-                stringFromApplicationContextTextView.text = requireActivity().applicationContext
-                    .getString(R.string.string_from_application_context)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) = Unit
-        }
-    }
-}
-
-fun Context.getStringResourceByName(id: String): String? {
-    val resId = resources.getIdentifier(id, "string", packageName)
-    return if (resId != 0) {
-        getString(resId)
-    } else {
-        null
     }
 }
